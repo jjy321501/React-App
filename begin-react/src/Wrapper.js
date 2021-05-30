3ddcaccf6cf4d9bfe2275011ef48705ef613068c
jjy321501 <jjy321501@naver.com>
2021-05-31 00:49:29 +0900

    React-app props, children 실습

--- begin-react/src/Wrapper.js
+++ begin-react/src/Wrapper.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
-function Wrapper(){
+//쓰임 당하는 곳에서 값을 정하는 것 => children
+function Wrapper({children}){
     const style = {
         border: '2px solid black',
         padding: '16px',
     };
     return(
         <div style={style}>
-
+            {children}
         </div>
     )
 }
