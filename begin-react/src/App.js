12cc6ec2780d36e637383427c58ab6b34dd301af
jjy321501 <jjy321501@naver.com>
2021-06-01 21:56:07 +0900

    React-app 조건부 렌더링 실습

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -5,24 +5,24 @@ import './App.css';
 
 
 function App() {
-  const name = 'react';
+  /* const name = 'react';
   const style = {
     backgroundColor: 'black',
     color: 'aqua',
     fontSize: 24, // 기본 단위 px
     padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
+  } */
 
   return (//쓰이는 곳에서 값을 정한다 => props(부모)
     <>
       {/* 주석은 화면에 보이지 않습니다 */}
       /* 중괄호로 감싸지 않으면 화면에 보입니다 */
       <Wrapper>
-        <Hello name="react" color="red"/>
+        <Hello name="react" color="red" isSpecial={true}/*{true} 를 생략하면 true 가 default 값*//>
         <Hello color="pink"/>
       </Wrapper>
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
+     {/*  <div style={style}>{name}</div>
+      <div className="gray-box"></div> */}
     </>
   );
 }
