a9645404d9d7df4f102fd611841c4fcaa188db00
jjy321501 <jjy321501@naver.com>
2021-06-04 23:47:25 +0900

    React-app 여러개의 input 요소 관리 2

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -6,7 +6,7 @@ function InputSample() {
         nickname: ''
     });
 
-    const [name, nickname] = inputs; // 비구조화 할당을 통해 값 추출
+    const {name, nickname} = inputs; // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
         const {value, name} = e.target;// 우선 e.target 에서 name, value 를 추출
@@ -24,12 +24,12 @@ function InputSample() {
 
     return(
         <div>
-            <input placeholder="이름" onChange={onChange} value={name}/>
-            <input placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <input name="name" placeholder="이름" onChange={onChange} value={name}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값:  </b>
-                이름 (닉네임)
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div> 
     )
