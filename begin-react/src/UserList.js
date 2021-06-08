2af0ad613ffc008215b06062421148432abf0fa4
jjy321501 <jjy321501@naver.com>
2021-06-08 16:57:06 +0900

    React-app 배열 렌더링하기 (key)

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -29,10 +29,13 @@ function UserList(){
     ];
     return (
         <div>
-            <User user={users[0]}/>
+            {/* <User user={users[0]}/>
             <User user={users[1]}/>
-            <User user={users[2]}/>
+            <User user={users[2]}/> */}
+            {users.map(user => ( //자바스크립트 배열의 내장함수 map 을 사용
+                <User user={user} key={user.id}/>//react에서 배열을 렌더링시 key props를 설정해야함(없다면 index)
+            ))}
         </div>
-    )
+    );
 }
 export default UserList;
