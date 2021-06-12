f14a81596331a4ef85db01708db8b628cae516af
jjy321501 <jjy321501@naver.com>
2021-06-12 22:26:49 +0900

    React-app UserList / 삭제버튼 렌더링

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user , onRemove }) {
 
     return (
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({users}){
+function UserList({users,onRemove}){
     
     return (
         <div>
@@ -17,7 +18,7 @@ function UserList({users}){
             <User user={users[1]}/>
             <User user={users[2]}/> */}
             {users.map(user => ( //자바스크립트 배열의 내장함수 map 을 사용
-                <User user={user} key={user.id}/>//react에서 배열을 렌더링시 key props를 설정해야함(없다면 index)
+                <User user={user} key={user.id} onRemove={onRemove}/>//react에서 배열을 렌더링시 key props를 설정해야함(없다면 index)
             ))}
         </div>
     );
