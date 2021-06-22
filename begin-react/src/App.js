9e431e2009308136b02213019aa9fed4d6a29218
jjy321501 <jjy321501@naver.com>
2021-06-22 23:20:02 +0900

    React-app useState, useReducer

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React,{useRef, useState, useMemo, useCallback} from 'react';
+import React,{useRef, useReducer, useMemo, useCallback} from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import './App.css';
@@ -13,30 +13,14 @@ function countActiveUsers(users){
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  /* const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  } */
-
-  const [inputs, setInputs] = useState({
-    username:'',
-    email:''
-  });
-  const {username, email} = inputs;
-  const onChange = useCallback(e => {
-    const {name, value} = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]:value
-    }));
-  },[]);
-  const [users,setUsers] = useState([
+const initialState = {
+  inputs: {
+    username: '',
+    email: ''
+  },
+  users:[
     {
-        id:1,
+      id:1,
         username:'velopert',
         email:'public.velopert@gmail.com',
         active:true
@@ -53,43 +37,101 @@ function App() {
         email:'liz@example.com',
         active:false
     }
-  ]);
+  ]
+};
 
+function reducer(state, action){
+  switch (action.type){
+    case 'CHANGE_INPUT':
+      return{
+        ...state,
+        inputs:{
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs:initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return{
+        ...state,
+        users:state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user  
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter( user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  /* const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  } */
+  const [state , dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
+
+  const {users} = state;
+  const {username, email} = state.inputs;
+  
+  const onChange = useCallback(e => {
+    const {name, value} = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  },[]);
+
   //함수형 업데이트 (리렌더링 최적화)
   const onCreate = useCallback(() => { 
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
+    dispatch({
+      type:'CREATE_USER',
+      user:{
+        id: nextId.current,
+        username,
+        email
+      }
+    });
     //Spread 방식
     /* setUsers([...users, user]); */
     //Concat 방식
-    setUsers(users.concat(user));
+    /* setUsers(users.concat(user));
 
     setInputs({
       username:'',
       email:''
-    });
+    }); */
     nextId.current += 1;
   },[username, email]);
+  
   //함수형 업데이트 (리렌더링 최적화)
-  const onRemove = useCallback(
-    id => {
-      //user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인것을 제거함
-      setUsers(users.filter(user => user.id !== id));//불변성 지키면서 배열에서 제거하기위해 filter 배열 내장함수를 사용
-    },[]);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  },[]);
   //함수형 업데이트 (리렌더링 최적화)
-  const onToggle = useCallback(
-    id => {
-      setUsers(
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    },[]);
+  const onRemove = useCallback(id => {
+    dispatch({
+      type:'REMOVE_USER',
+      id
+    });
+  },[]);
+
   const count = useMemo(() => countActiveUsers(users),[users]);
   return (//쓰이는 곳에서 값을 정한다 => props(부모)
     <>
