cb12e9aa04e3650b18314956fbb6da7e82608e68
jjy321501 <jjy321501@naver.com>
2021-06-28 21:05:11 +0900

    React-app 클래스형 컴포넌트

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({color, name, isSpecial}) {
-  return (
-    <div style={{ color }}>
-        {isSpecial ? <b>*</b> : null}
-        안녕하세요 {name}
-    </div>
-    );
-}
-
-Hello.defaultProps = {
-    name: '이름없음'
+class Hello extends Component{
+    static defaultProps = {
+        name: '이름없음'
+    };
+    render() {//Class 형 컴포넌트에서는 render() 메소드가 필수이다.
+    const {color, name, isSpecial } = this.props;
+    return (
+        <div style={{ color }}>
+            {isSpecial ? <b>*</b> : null}
+            안녕하세요 {name}
+        </div>    
+    ); 
+  }
 }
 
 export default Hello;
