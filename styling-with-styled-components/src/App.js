cf580f55d9f6343f20170c04f8092f41ec3e34a2
jjy321501 <jjy321501@naver.com>
2021-07-17 01:27:23 +0900

    Button 컴포넌트 fullWidth 스타일

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -23,40 +23,59 @@ function App() {
         palette: {
           blue: '#228be6',
           gray: '#495057',
-          pink: '#f06597'
+          pink: '#f06595'
         }
       }}
     >
-    <AppBlock>
-      <ButtonGroup>
-        <Button size="large">Button</Button>
-        <Button>Button</Button>
-        <Button size="small">Button</Button>
-      </ButtonGroup>
-      <ButtonGroup>
-        <Button color="gray" size="large">Button</Button>
-        <Button color="gray" >Button</Button>
-        <Button color="gray" size="small">Button</Button>
-      </ButtonGroup>
-      <ButtonGroup>
-        <Button color="pink" size="large">Button</Button>
-        <Button color="pink" >Button</Button>
-        <Button color="pink" size="small">Button</Button>
-      </ButtonGroup>
-      <ButtonGroup>
-        <Button size="large" outline>
-          BUTTON
-        </Button>
-        <Button color="gray" outline>
-          BUTTON
-        </Button>
-        <Button color="pink" outline>
-          BUTTON
-        </Button>
-      </ButtonGroup>
-    </AppBlock>
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="small">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="gray" size="large">
+            BUTTON
+          </Button>
+          <Button color="gray">BUTTON</Button>
+          <Button color="gray" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="pink" size="large">
+            BUTTON
+          </Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="pink" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" outline>
+            BUTTON
+          </Button>
+          <Button color="gray" outline>
+            BUTTON
+          </Button>
+          <Button color="pink" size="small" outline>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="gray" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="pink" fullWidth>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
--- styling-with-styled-components/src/components/Button.js
+++ styling-with-styled-components/src/components/Button.js
@@ -3,31 +3,29 @@ import styled, { css } from 'styled-components';
 import { darken, lighten } from 'polished';
 
 const colorStyles = css`
- ${({ theme, color }) => {
-   const selected = theme.palette[color];
-   return css`
-    background: ${selected};
-    &:hover {
-      background: ${lighten(0.1, selected)};
-    }
-    &:active {
-      background: ${darken(0.1, selected)};
-    }
-  `;
- }}
-`;
-const AppBlock = styled.div`
-  width: 512px;
-  margin: 0 auto;
-  margin-top: 4rem;
-  border: 1px solid black;
-  padding: 1rem;
-`;
-
-const ButtonGroup = styled.div`
-  & + & {
-    margin-top: 1rem;
-  }
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+      ${props =>
+        props.outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
+    `;
+  }}
 `;
 
 const sizes = {
@@ -52,41 +50,62 @@ const sizeStyles = css`
   `}
 `;
 
-const StyledButton = styled.button`
-    /* 공통 스타일 */
-    display: inline-flex;
-    outline: none;
-    border: none;
-    border-radius: 4px;
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-    padding-left: 1rem;
-    padding-right: 1rem;
-
-    /* 크기 */
-    ${sizeStyles}
-
-    /* 색상 */
-    ${colorStyles}
-
-    /* 기타 */
-    & + & {
-        margin-left: 1rem;
-    }
+const fullWidthStyle = css`
+  ${props =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+      justify-content: center;
+      & + & {
+        margin-left: 0;
+        margin-top: 1rem;
+      }
+    `}
 `;
 
-function Button({ children, color, size, ...rest }){
-    return (
-        <StyledButton color={color} size={size} {...rest}>
-            {children}
-        </StyledButton>
-    );
+const StyledButton = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* 크기 */
+  ${sizeStyles}
+
+  /* 색상 */
+  ${colorStyles}
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+
+  ${fullWidthStyle}
+`;
+
+function Button({ children, color, size, outline, fullWidth, ...rest }) {
+  return (
+    <StyledButton
+      color={color}
+      size={size}
+      outline={outline}
+      fullWidth={fullWidth}
+      {...rest}
+    >
+      {children}
+    </StyledButton>
+  );
 }
 
 Button.defaultProps = {
-    color: 'blue',
-    size: 'medium'
+  color: 'blue',
+  size: 'medium'
 };
 
 export default Button;
